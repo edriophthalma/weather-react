f71d1d5e75c597940fddd4516efdc3403b05792f
edriophthalma <86781480+edriophthalma@users.noreply.github.com>
2021-10-28 19:17:01 +0100

    Weather-react structure

--- src/Weather.js
+++ src/Weather.js
@@ -1,19 +1,105 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import Loader from "react-loader-spinner";
 
-export default function Weather(props) {
-    function handleWeather(response) {
-        alert(`the weather in ${response.data.name} is just fine`);
-    }
+
+export default function Weather() {
+  const [city, setCity] = useState("");
+  const [result, setResult] = useState(false);
+  const [info, setInfo] = useState("");
+
+  function displayWeather(response) {
+    setResult(true);
+
+    setInfo({
+        name: response.data.name,
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    });
+  }
+
+  function dataSearch(event) {
+    event.preventDefault();
     let apiKey = "c5e04ad803ec417da869647e91a10830";
-    let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
-        axios.get(apiUrl).then(handleWeather);
-return <header><h1>Weather</h1>
- <Loader
-        type="Puff"
-        color="#00BFFF"
-        height={100}
-        width={100}
-        timeout={6000} //3 secs
-      /></header>;}
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+  let form = (
+      <table><tr> <td>
+    <form onSubmit={dataSearch}>
+       <input type="search" placeholder="Type a city" onChange={updateCity} />
+      <input type="submit" value="Search" />
+    </form></td>
+    <td> 
+        <h3 className="city-result">{info.name}</h3></td></tr></table>
+  );
+
+  if (result) {
+    return (
+      <div>
+        {form}
+        <table>
+            <tr>
+            <td>
+                <ul>
+              <li><img src={info.icon} alt="icon" /></li>
+              <li>{info.description}</li>  
+              </ul>
+            </td>
+            <td>
+              <p>
+      <ul className="info-weather">
+              <li>Temperature: {Math.round(info.temperature)}ºC</li>
+            <li>Windspeed: {info.wind}Km/h</li>
+              <li>Humidity: {info.humidity}%</li>
+              
+              </ul>
+              </p>
+              </td></tr>
+          </table>
+          <br/>
+          <table>
+              <tr >
+                <td><ul>
+                    <li>Fri</li>
+                    <li><img src={info.icon} alt="icon" /></li>
+                    <li>{info.description}</li>  
+                    </ul>
+                    </td>
+                <td> <ul>
+                    <li>Sat</li>
+                    <li><img src={info.icon} alt="icon" /></li>
+                    <li>{info.description}</li>  
+                    </ul>
+                    </td>
+                <td> <ul>
+                    <li>Sun</li>
+                    <li><img src={info.icon} alt="icon" /></li>
+                     <li>{info.description}</li>  
+                     </ul></td>
+                 <td><ul>
+                     <li>Mon</li>
+                    <li><img src={info.icon} alt="icon" /></li>
+                     <li>{info.description}</li>  
+                    </ul></td>
+                 <td><ul>
+                     <li>Tue</li>
+                    <li><img src={info.icon} alt="icon" /></li>
+                     <li>{info.description}</li>  
+                     </ul></td>
+              </tr>
+          </table>
+        
+    
+    </div>
+    ); }
+  else {
+    return form;
+  }
+}
